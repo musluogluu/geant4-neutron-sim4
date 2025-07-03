7bcda08756f722502defa3f9c410b36f3d4567ac
musluogluu <musluoglu.mert10@gmail.com>
2025-07-03 21:12:45 +0300

    strange

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -1,13 +1,13 @@
 #include "SteppingAction.hh"
-#include "RunAction.hh" // RunAction'ın tam tanımı gerekli
+#include "RunAction.hh"
 
 #include "G4Step.hh"
 #include "G4Track.hh"
 #include "G4VPhysicalVolume.hh"
 #include "G4LogicalVolume.hh"
 #include "G4UnitsTable.hh"
-#include "G4SystemOfUnits.hh" // Birimler için
-#include "G4Event.hh" // EventID için
+#include "G4SystemOfUnits.hh"
+#include "G4Event.hh"
 
 SteppingAction::SteppingAction(RunAction* runAction)
 : G4UserSteppingAction(),
@@ -25,20 +25,22 @@ void SteppingAction::UserSteppingAction(const G4Step* aStep)
 
     if (volume->GetLogicalVolume()->GetName() == "LeadCylinder") {
         G4Track* track = aStep->GetTrack();
+        G4Event* currentEvent = G4EventManager::GetInstance()->GetCurrentEvent();
         std::ofstream& output = fRunAction->GetOutputFile();
 
-        output << aStep->GetTrack()->GetCurrentEvent()->GetEventID() << ",";
+        // Verileri dosyaya CSV formatında yaz
+        output << currentEvent->GetEventID() << ","; // Burası düzelt
         output << track->GetDefinition()->GetParticleName() << ",";
         output << track->GetTrackID() << ",";
         output << track->GetParentID() << ",";
-        output << G4BestUnit(track->GetKineticEnergy(), "Energy") << ","; // MeV cinsinden
-        output << G4BestUnit(aStep->GetTotalEnergyDeposit(), "Energy") << ","; // MeV cinsinden
-        output << G4BestUnit(track->GetGlobalTime(), "Time") << ","; // ns cinsinden
-        output << G4BestUnit(track->GetLocalTime(), "Time") << ","; // ns cinsinden
-        output << G4BestUnit(aStep->GetStepLength(), "Length") << ","; // mm cinsinden
-        output << G4BestUnit(track->GetPosition().x(), "Length") << ","; // mm cinsinden
-        output << G4BestUnit(track->GetPosition().y(), "Length") << ","; // mm cinsinden
-        output << G4BestUnit(track->GetPosition().z(), "Length") << ","; // mm cinsinden
+        output << G4BestUnit(track->GetKineticEnergy(), "Energy") << ",";
+        output << G4BestUnit(aStep->GetTotalEnergyDeposit(), "Energy") << ",";
+        output << G4BestUnit(track->GetGlobalTime(), "Time") << ",";
+        output << G4BestUnit(track->GetLocalTime(), "Time") << ",";
+        output << G4BestUnit(aStep->GetStepLength(), "Length") << ",";
+        output << G4BestUnit(track->GetPosition().x(), "Length") << ",";
+        output << G4BestUnit(track->GetPosition().y(), "Length") << ",";
+        output << G4BestUnit(track->GetPosition().z(), "Length") << ",";
         output << volume->GetName() << "\n";
     }
 }
